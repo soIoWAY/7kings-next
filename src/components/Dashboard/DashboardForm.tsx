bf65ed357fad88039c446329ae4dce65ef7e0ba9
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-05-09 21:11:55 +0300

    adaptive dashboard

--- src/components/Dashboard/DashboardForm.tsx
+++ src/components/Dashboard/DashboardForm.tsx
@@ -4,12 +4,12 @@ interface IDashboardForm {
 
 const DashboardForm = ({ title }: IDashboardForm) => {
 	return (
-		<div>
+		<div className='flex flex-col gap-2'>
 			<h2 className='font-bold text-xl'>{title}</h2>
-			<div className='flex gap-3 items-center'>
+			<div className='flex gap-3 items-center flex-col sm:flex-row'>
 				<input
 					type='text'
-					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-2 text-lg w-2/12'
+					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-1 sm:p-2 text-lg w-full sm:w-2/12'
 				/>
 				<button className='bg-green-800 border border-green-400 rounded-md p-2'>
 					{title}
--- src/components/Dashboard/DashboardTransferForm.tsx
+++ src/components/Dashboard/DashboardTransferForm.tsx
@@ -1,16 +1,16 @@
 const DashboardTransferForm = () => {
 	return (
-		<div>
+		<div className='flex flex-col gap-2'>
 			<h2 className='font-bold text-xl'>Transfer money to player</h2>
-			<div className='flex gap-3 items-center'>
+			<div className='flex gap-3 items-center flex-col sm:flex-row'>
 				<input
 					type='text'
-					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-2 text-lg w-1/12'
+					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-2 text-lg w-1/2 sm:w-1/12'
 					placeholder='sum'
 				/>
 				<input
 					type='text'
-					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-2 text-lg w-2/12'
+					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-2 text-lg w-full sm:w-2/12'
 					placeholder='username'
 				/>
 				<button className='bg-green-800 border border-green-400 rounded-md p-2'>
