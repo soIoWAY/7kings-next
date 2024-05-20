9f70b023ace5be797e5793d6555f08eb3f4b6167
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-05-20 22:02:40 +0300

    upd forms

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -1,13 +1,15 @@
 interface IDashboardCard {
 	title: string
 	data: string
+	textColor: string
 }
 
-const DashboardCard = ({ title, data }: IDashboardCard) => {
+const DashboardCard = ({ title, data, textColor }: IDashboardCard) => {
+	const dataStyles = `text-xl font-bold ${textColor}`
 	return (
 		<div className='bg-[#0d0d0d] rounded-md max-w-fit py-2 px-4 flex flex-col'>
 			<span className='text-sm'>{title}</span>
-			<span className='text-xl font-bold'>{data}</span>
+			<span className={dataStyles}>{data}</span>
 		</div>
 	)
 }
--- src/components/Dashboard/DashboardForm.tsx
+++ src/components/Dashboard/DashboardForm.tsx
@@ -1,20 +1,56 @@
+import { userStore } from '@/store/user'
+import { updateUserInfo } from '@/utils/userUpdate'
+import { useState } from 'react'
+
 interface IDashboardForm {
 	title: string
+	userLevel: string
 }
 
-const DashboardForm = ({ title }: IDashboardForm) => {
+const DashboardForm = ({ title, userLevel }: IDashboardForm) => {
+	const [promocode, setPromocode] = useState('')
+	const userLevelToNumber = parseInt(userLevel)
+	const sendFormHandler = async (e: React.FormEvent) => {
+		e.preventDefault()
+		try {
+			updateUserInfo(undefined, undefined, undefined, undefined, promocode)
+			userStore.setState({ promocode: promocode })
+		} catch (error) {
+			console.error(error)
+		}
+		setPromocode('')
+	}
+	const buttonStyles = 'border rounded-md p-2 bg-green-800 border-green-400'
+	const disabledButtonStyles = 'border rounded-md p-2 bg-red-500 border-red-400'
 	return (
 		<div className='flex flex-col gap-2'>
-			<h2 className='font-bold text-xl'>{title}</h2>
-			<div className='flex gap-3 items-center flex-col sm:flex-row'>
+			<h2 className='font-bold text-xl'>
+				{`${title}`} (
+				<span className='underline'>
+					min lvl <span className='text-green-500'>3</span>
+				</span>
+				)
+			</h2>
+			<form
+				className='flex gap-3 items-center flex-col sm:flex-row'
+				onSubmit={sendFormHandler}
+			>
 				<input
 					type='text'
 					className='border-green-500 border bg-[#0d0d0d] rounded-md outline-none p-1 sm:p-2 text-lg w-full sm:w-2/12'
+					maxLength={12}
+					value={promocode}
+					onChange={e => setPromocode(e.target.value)}
 				/>
-				<button className='bg-green-800 border border-green-400 rounded-md p-2'>
-					{title}
-				</button>
-			</div>
+				<input
+					type='submit'
+					className={
+						userLevelToNumber >= 3 ? buttonStyles : disabledButtonStyles
+					}
+					value={title}
+					disabled={userLevelToNumber >= 3 ? false : true}
+				/>
+			</form>
 		</div>
 	)
 }
