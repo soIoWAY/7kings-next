98725d42c208c56c5adc6917c539b58b99ce754c
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-05-20 21:59:07 +0300

    add promo to user store

--- src/store/user.ts
+++ src/store/user.ts
@@ -14,5 +14,6 @@ export const userStore = create(async set => {
 		balance: data.balance || 0,
 		username: data.username || '',
 		level: data.level || 0,
+		promocode: data.promocode || '',
 	})
 })
