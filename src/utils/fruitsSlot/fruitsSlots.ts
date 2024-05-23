299e42422646bb487ae3192f68ef376eb7f8e89f
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-05-23 11:31:57 +0300

    new cherry logic

--- src/utils/fruitsSlot/fruitsSlots.ts
+++ src/utils/fruitsSlot/fruitsSlots.ts
@@ -8,6 +8,26 @@ const lvlWinMultipliers: { [key: number]: number } = {
 	5: 1.07,
 }
 
+const winUpdate = (
+	userBalance: number,
+	userBet: number,
+	x: number,
+	multiplier: number,
+	userWins: number
+) => {
+	const increasedBalance = userBalance + userBet * x * multiplier
+	const newWins = userWins + 1
+	updateUserInfo(newWins, undefined, increasedBalance, undefined)
+	userStore.setState({ wins: newWins, balance: increasedBalance })
+}
+
+const loseUpdate = (userLoses: number) => {
+	const newLoses = userLoses + 1
+	userStore.setState({ loses: newLoses })
+	updateUserInfo(undefined, newLoses, undefined, undefined)
+}
+
+// bomb logic
 const fruitsChecker = (
 	fruits: string[],
 	userBalance: number,
@@ -17,17 +37,56 @@ const fruitsChecker = (
 	userLoses: number
 ) => {
 	const multiplier = lvlWinMultipliers[userLevel] || 1
-	// const decreasedBalance = userBalance - userBet
-	if (fruits[0] === fruits[1] && fruits[1] === fruits[2]) {
-		const increasedBalance = userBalance + userBet * 5 * multiplier
-		const newWins = userWins + 1
-		updateUserInfo(newWins, undefined, increasedBalance, undefined)
-		userStore.setState({ wins: newWins, balance: increasedBalance })
-	} else if (fruits[0] === fruits[1] || fruits[1] === fruits[2]) {
-		const newWins = userWins + 1
-		const increasedBalance = userBalance + userBet * 2 * multiplier
-		updateUserInfo(newWins, undefined, increasedBalance, undefined)
-		userStore.setState({ wins: newWins, balance: increasedBalance })
+	const fruitMultipliers = {
+		'🍒': 10,
+		'🍏': 8,
+		'🍋': 7,
+		'🍌': 6,
+		'🍇': 5,
+	}
+	if (fruits.every(fruit => fruit === '🍒')) {
+		winUpdate(userBalance, userBet, 10, multiplier, userWins)
+	} else if (fruits.every(fruit => fruit === '🍏')) {
+		winUpdate(userBalance, userBet, 8, multiplier, userWins)
+	} else if (fruits.every(fruit => fruit === '🍋')) {
+		winUpdate(userBalance, userBet, 7, multiplier, userWins)
+	} else if (fruits.every(fruit => fruit === '🍌')) {
+		winUpdate(userBalance, userBet, 6, multiplier, userWins)
+	} else if (fruits.every(fruit => fruit === '🍇')) {
+		winUpdate(userBalance, userBet, 5, multiplier, userWins)
+	} else if (fruits[0] === '💣' || fruits[1] === '💣' || fruits[2] === '💣') {
+		loseUpdate(userLoses) // lose
+	} else if (
+		(fruits[0] === '🍒' && fruits[1] === fruits[0]) ||
+		(fruits[1] === '🍒' && fruits[2] === fruits[1])
+	) {
+		winUpdate(userBalance, userBet, 4, multiplier, userWins)
+	} else if (
+		(fruits[0] === '🍏' && fruits[1] === fruits[0]) ||
+		(fruits[1] === '🍏' && fruits[2] === fruits[1])
+	) {
+		winUpdate(userBalance, userBet, 3.5, multiplier, userWins)
+	} else if (
+		(fruits[0] === '🍋' && fruits[1] === fruits[0]) ||
+		(fruits[1] === '🍋' && fruits[2] === fruits[1])
+	) {
+		winUpdate(userBalance, userBet, 3, multiplier, userWins)
+	} else if (
+		(fruits[0] === '🍌' && fruits[1] === fruits[0]) ||
+		(fruits[1] === '🍌' && fruits[2] === fruits[1])
+	) {
+		winUpdate(userBalance, userBet, 2.5, multiplier, userWins)
+	} else if (
+		(fruits[0] === '🍇' && fruits[1] === fruits[0]) ||
+		(fruits[1] === '🍇' && fruits[2] === fruits[1])
+	) {
+		winUpdate(userBalance, userBet, 2, multiplier, userWins)
+	} else if (
+		fruits[0] !== fruits[1] &&
+		fruits[1] !== fruits[2] &&
+		fruits[0] !== fruits[2]
+	) {
+		winUpdate(userBalance, userBet, 1.5, multiplier, userWins)
 	} else {
 		const newLoses = userLoses + 1
 		userStore.setState({ loses: newLoses })
