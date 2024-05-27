ce19e02ef3bb589b9f4ff79f150eb8b0fa29417a
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-05-27 22:35:37 +0300

    fix balance

--- src/utils/formattedBalanceToK.ts
+++ src/utils/formattedBalanceToK.ts
@@ -1,6 +1,6 @@
 export const formattedBalanceToK = (balance: number) => {
 	if (balance < 1000) {
-		return balance.toString()
+		return balance.toFixed(2)
 	} else if (balance < 1000000) {
 		return (balance / 1000).toFixed(1) + 'k'
 	} else {
